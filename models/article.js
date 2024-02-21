0bf364e9a156f4554add4107a988607f3d6ee0c0
Divya B <divyaaa1812@gmail.com>
2024-02-20 22:12:09 -0800

    created schema models

---
+++ models/article.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+// define clothing item schema
+const newsarticles = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => validator.isURL(value),
+      message: "Link is not valid",
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => validator.isURL(value),
+      message: "Link is not valid",
+    },
+  },
+  owner: {
+    required: true,
+    type: mongoose.ObjectId,
+    ref: "user",
+  },
+});
+
+module.exports = mongoose.model("clothingitems", clothingitems);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "Enter valid Email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
