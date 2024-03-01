d24a951d1471d6341ac69b562a837db2d4577b3f
Divya B <divyaaa1812@gmail.com>
2024-03-01 10:46:59 -0800

    changes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const mongoose = require("mongoose");
+require("dotenv").config();
+
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const routes = require("./routes");
+
+const app = express();
+const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/newsxplorer_db", () => {
   console.log("connected to DB");
@@ -9,3 +13,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/newsxplorer_db", () => {
 
 app.use(express.json());
 app.use(routes);
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
