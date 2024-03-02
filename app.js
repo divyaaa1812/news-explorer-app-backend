f189739c1f29fbe7fb34433c5388db53d2079be6
Divya B <divyaaa1812@gmail.com>
2024-03-01 22:59:20 -0800

    error/request loggers

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -12,7 +12,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/newsxplorer_db", () => {
 });
 
 app.use(express.json());
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
