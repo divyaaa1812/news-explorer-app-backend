9ec49b21c272b21f84bf1d4e1f960aaf252a05ea
Divya B <divyaaa1812@gmail.com>
2024-03-08 12:38:14 -0800

    fix

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,3 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
-
-const token = jwt.sign(
-  { _id: user._id },
-  NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
-);
+const { JWT_SECRET } = process.env;
 
 module.exports = { JWT_SECRET };
