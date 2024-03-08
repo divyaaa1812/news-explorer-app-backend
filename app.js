6d2258bafdb7d35d7416fe028b318fe059c63209
Divya B <divyaaa1812@gmail.com>
2024-03-08 11:43:51 -0800

    fix

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const routes = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { errors } = require("celebrate");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 const { PORT = 3002 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // in 15 minutes
+  max: 100, // you can make a maximum of 100 requests from one IP
+});
 
 console.log(process.env.NODE_ENV);
 
@@ -25,6 +30,8 @@ app.use(errorLogger);
 app.use(errors());
 // centralized error handler
 app.use(errorHandler);
+// applying the rate-limiter
+app.use(limiter);
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
