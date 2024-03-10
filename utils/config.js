6131d8539fdec685b1d73d247b20bf22be6b05c7
Divya B <divyaaa1812@gmail.com>
2024-03-10 11:44:25 -0700

    fix

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
-const { JWT_SECRET_PROD } = process.env;
-
 const JWT_SECRET =
-  process.env.NODE_ENV === "production" ? JWT_SECRET_PROD : "Secret-Dev-key";
+  process.env.NODE_ENV === "production"
+    ? process.env.JWT_SECRET
+    : "Secret-Dev-key";
 
 module.exports = { JWT_SECRET };
