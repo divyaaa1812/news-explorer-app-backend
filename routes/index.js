aac5108825368945ce998f234f0b9ca014ee50fb
Divya B <divyaaa1812@gmail.com>
2024-03-26 20:06:49 -0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ const handleAuthorization = require("../middlewares/auth");
 router.use("/users", handleAuthorization, users);
 router.use("/articles", articles);
 
-router.post("/signup", validateCreateUser, createUser);
+// router.post("/signup", validateCreateUser, createUser);
+router.post("/signup", createUser);
 router.post("/signin", validateUserLogin, loginUser);
 
 router.use((req, res, next) => {
