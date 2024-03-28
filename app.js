bdfe24409e6146306ac7c99efd06d283080c8c11
Divya B <divyaaa1812@gmail.com>
2024-03-27 18:44:37 -0700

    test

--- app.js
+++ app.js
@@ -19,32 +19,6 @@ console.log(process.env.NODE_ENV);
 //     : "mongodb://127.0.0.1:27017/newsxplorer_db";
 const dbUrl = "mongodb://127.0.0.1:27017/newsxplorer_db";
 
-console.log(dbUrl);
-console.log(process.env);
-// app.get("/crash-test", () => {
-//   setTimeout(() => {
-//     throw new Error("Server will crash now");
-//   }, 0);
-// });
-
-// app.use((req, res, next) => {
-//   let allowedOrigins = [
-//     "https://nx.csproject.org",
-//     "https://wtwrdivya.crabdance.com",
-//   ];
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
-//   }
-//   // Handle preflight requests
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
-
 mongoose.connect(dbUrl, () => {
   console.log("connected to DB");
 });
